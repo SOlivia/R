0caac12b940e37024422e695c9c1f54c73bf5481
SOlivia <s.muchchima@gmail.com>
2017-01-17 20:17:56 -0800

    Add files via upload

---
+++ fscope.r
@@ -0,0 +1,10 @@
+y<-7
+fscope <- function(x) {     
+	print(paste("y = ",y))                     
+	y <- 2*x                  
+	w <- 3*x                 
+	print(paste("w = ",w))
+	print(paste("x = ",x))
+	print(paste("y = ",y))
+	print(paste("z = ",z))    
+}
